47eb1f3174776ce57ec1fa2dab10c5e150a2b235
ayman-a275 <ayman.dk01@gmail.com>
2025-05-15 11:14:34 +0200

    Trip

---
+++ CarLibrary/Trip.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarLibrary
+{
+    public class Trip
+    {
+        public double Distance { get; set; }
+        public DateTime TripDate { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public Car Car { get; set; }
+
+        public Trip(double distance, DateTime tripDate, DateTime startTime, DateTime endTime, Car car)
+        {
+            Distance = distance;
+            TripDate = tripDate;
+            StartTime = startTime;
+            EndTime = endTime;
+            Car = car;
+        }
+
+        public TimeSpan CalculateDuration()
+        {
+            return EndTime - StartTime;
+        }
+
+        public double CalculateTripPrice(double energyUnitPrice)
+        {
+            return Distance * energyUnitPrice;
+        }
+
+        public void PrintTripDetails()
+        {
+            Console.WriteLine($@"Distance: {Distance}
+Trip date: {TripDate}
+Start time: {StartTime}
+End time: {EndTime}
+Bil: {Car.Brand} {Car.Model}");
+        }
+    }
+}
